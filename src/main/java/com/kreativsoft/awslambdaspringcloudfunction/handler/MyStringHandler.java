e4b85e3872f7191f085eef53e95af843790c3e4f
Nishantha Bulumulla <nishanthabulumulla@sml.com>
2022-11-24 10:16:14 +0530

    MyStringHandler was formatted

--- src/main/java/com/kreativsoft/awslambdaspringcloudfunction/handler/MyStringHandler.java
+++ src/main/java/com/kreativsoft/awslambdaspringcloudfunction/handler/MyStringHandler.java
@@ -1,6 +1,7 @@
 package com.kreativsoft.awslambdaspringcloudfunction.handler;
 
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-public class MyStringHandler extends SpringBootRequestHandler<String,Object> {
+
+public class MyStringHandler extends SpringBootRequestHandler<String, Object> {
 }
 
